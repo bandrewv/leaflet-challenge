d65a6dc23f14ef705dc49ca5d357da8608bf80fb
bandrewv <brock.vriesman@gmail.com>
2020-10-25 11:54:21 -0700

    Can't figure out choropleth.

--- static/js/logic.js
+++ static/js/logic.js
@@ -7,44 +7,6 @@ d3.json(queryURL).then(data => {
 
     createFeatures(data.features);
 
-    // Add a choropleth layer
-    let geojson = L.choropleth(data, {
-        valueProperty: 'mag', // which property in the features to use
-        scale: ['green', 'red'], // chroma.js scale - include as many as you like
-        steps: 6, // number of breaks or steps in range
-        mode: 'q', // q for quantile, e for equidistant, k for k-means
-        style: {
-            color: '#fff', // border color
-            weight: 2,
-            fillOpacity: 0.8
-        }
-    }).addTo(myMap);
-
-
-    // // Set up the legend
-    // var legend = L.control({ position: "bottomright" });
-    // legend.onAdd = function() {
-    // var div = L.DomUtil.create("div", "info legend");
-    // var limits = geojson.options.limits;
-    // var colors = geojson.options.colors;
-    // var labels = [];
-
-    // // Add min & max
-    // var legendInfo = `<h1>Depth</h1>
-    //     <div class="labels">
-    //     <div class="min"> ${limits[0].toLocaleString(undefined,{style:'currency',currency:'USD',maximumSignificantDigits: 3})} </div>
-    //     <div class="max"> ${limits[limits.length - 1].toLocaleString(undefined,{style:'currency',currency:'USD',maximumSignificantDigits: 4})} </div>
-    //     </div>`;
-
-    // div.innerHTML = legendInfo;
-
-    // limits.forEach(function(limit, index) {
-    //     labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-    // });
-
-    // div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-    // return div;
-    // };
 });
 
 
@@ -56,9 +18,9 @@ function createFeatures(earthquakeData) {
         feature.properties.place);
     }
 
-    // var earthquakes = L.geoJSON(earthquakeData, {
-    //     onEachFeature: onEachFeature,
-    // });
+    var earthquakes = L.geoJSON(earthquakeData, {
+        onEachFeature: onEachFeature,
+    });
 
     var mags = L.geoJSON(earthquakeData, {
         onEachFeature: onEachFeature,
@@ -72,11 +34,11 @@ function createFeatures(earthquakeData) {
     });
 
     // Adding earthquake layer to createMap function
-    createMap(mags);
+    createMap(earthquakes, mags);
 }
 
 
-function createMap(mags) {
+function createMap(earthquakes, mags) {
 
     // Define streetmap layer
     var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -95,7 +57,7 @@ function createMap(mags) {
 
     // Create overlay object to hold overlay layer
     var overlayMaps = {
-        // Earthquakes: earthquakes,
+        Earthquakes: earthquakes,
         Magnitude: mags
     };
 
@@ -107,7 +69,7 @@ function createMap(mags) {
     });
 
     // Create layer control
-    // L.control.layers(baseMaps, overlayMaps, {
-    //     collapsed: false
-    // }).addTo(myMap);
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap);
 };
