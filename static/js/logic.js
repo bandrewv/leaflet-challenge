3fb2dd55ca8ede3d8e12a651c7f2684e7f5652fc
bandrewv <brock.vriesman@gmail.com>
2020-10-24 22:23:07 -0700

    Conected data to project.

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,30 @@
+// Store API endpoint in variable
+const queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+// Perform GET request to queryURL
+d3.json(queryURL).then(data => {
+    console.log(data);
+
+    // createFeatures(data.features);
+
+    console.log(data.features)
+})
+
+
+// function createFeatures(earthquakeData) {
+
+//     function onEachFeature(feature, layer) {
+//         layer.bindPopup("<h3>" + feature.properties.title +
+//         "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+//     }
+
+// }
+
+
+
+
+
+
 // Create map object
 var myMap = L.map("map", {
     center: [15, 0],
@@ -13,6 +40,3 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   id: "mapbox/streets-v11",
   accessToken: API_KEY
 }).addTo(myMap);
-
-
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson")
